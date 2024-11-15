dff6a4f7bbcaa118616e77614e5cc815669da4c6
Nikiitanskiy <nik1308958@gmail.com>
2024-11-16 01:00:44 +0200

    Was wrote interface for program

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 import random
-import tkinter
+import customtkinter
+
+
+
+app = customtkinter.CTk()
+
+app.geometry("400x100")
+app.title("Password generator")
+
+
+
+#Function to generate password
+def generate_pass():
+    return None
+
+
+generate_button = customtkinter.CTkButton(app, text="Generate", command=generate_pass)
+generate_button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+
+check_special = customtkinter.CTkCheckBox(app, text="Special characters")
+check_special.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
+
+pass_label = customtkinter.CTkLabel(app, text="CLICK GENERATE")
+pass_label.grid(row=0, column=2, padx=20, pady=20)
+
+if __name__ == "__main__":
+    app.mainloop()
+
+
 
