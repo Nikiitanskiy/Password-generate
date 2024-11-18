a3069fd09678924d1d1cfbb50f7575c24cebdce1
Nikiitanskiy <nik1308958@gmail.com>
2024-11-18 23:43:03 +0200

    Version 1.0.0

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 import random
 import customtkinter
 
-alphabet_syms="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:',.<>?/"
+alphabet_syms="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+alphabet_spec_syms = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:',.<>?/"
 
 #Function to generate password
 def generate_pass(specialsymbols=True):
-    list1=[]
-    str_to_return = ""
-    for i in range(10):
-        if specialsymbols == True:
-            list1.append(alphabet_syms[random.randint(0,len(alphabet_syms)-1)])
-    for el in list1:
-        str_to_return+=str(el)
-    return str_to_return
-
-
+    password = ""
+    if specialsymbols == True:
+        alphabet = alphabet_spec_syms
+    else:
+        alphabet = alphabet_syms
+    for i in range (10):
+        password += random.choice(alphabet)
+    return password
+           
+#Function to change password in label
+def change_label():
+    special_sym = check_special.get()
+    password = generate_pass(specialsymbols=special_sym)
+    pass_label.configure(text = password)
+    
 
 app = customtkinter.CTk()
 
 app.geometry("400x100")
 app.title("Password generator")
-
+#Lock window`s size
 app.minsize(400,100)
 app.maxsize(400,100)
 
-
-
-
 #Button that generetad password
-generate_button = customtkinter.CTkButton(app, text="Generate", command=generate_pass)
+generate_button = customtkinter.CTkButton(app, text="Generate", command=change_label)
 generate_button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
 #Checkbox for checking special symbols in pass
 check_special = customtkinter.CTkCheckBox(app, text="Special characters")
@@ -39,6 +42,3 @@ pass_label.grid(row=0, column=2, padx=20, pady=20)
 
 if __name__ == "__main__":
     app.mainloop()
-    print(generate_pass(True))
-
-
